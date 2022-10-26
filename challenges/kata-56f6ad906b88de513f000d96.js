c2e2a5a79524a54da4d402461243a0248887d7e6
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-10-26 21:29:48 +0100

    add kata 56f6ad906b88de513f000d96

---
+++ challenges/kata-56f6ad906b88de513f000d96.js
@@ -0,0 +1,6 @@
+// link: https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+
+//my solution:
+function bonusTime(salary, bonus) {
+  return bonus ? `£${salary * 10}` : `£${salary}`;
+}
