da79a1782277c6fc9517a9529e0685866e3f2b3f
Haroon <haroon.ali.1104@gmail.com>
2023-04-29 18:18:03 +0100

    add kata 569e09850a8e371ab200000b

---
+++ challenges/kata-569e09850a8e371ab200000b.js
@@ -0,0 +1,12 @@
+// link: https://www.codewars.com/kata/569e09850a8e371ab200000b/train/javascript
+
+//my solution:
+function preFizz(n) {
+    const numbers = [];
+    
+    for (let i = 1; i <= n; i++) {
+      numbers.push(i);
+    }
+    
+    return numbers;
+  }
