643c50c450a85158f550064a81b44f77935b5435
Haroon <haroon.ali.1104@gmail.com>
2023-09-06 17:08:47 +0100

    add kata 525f50e3b73515a6db000b83

---
+++ challenges-3/kata-525f50e3b73515a6db000b83.js
@@ -0,0 +1,6 @@
+// link: https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+
+//my solution:
+function createPhoneNumber(numbers) {
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+}
