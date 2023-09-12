a898ef7b53c0dab6525e058937bc674f792a93f5
Haroon <haroon.ali.1104@gmail.com>
2023-09-12 17:34:14 +0100

    add kata 514b92a657cdc65150000006

---
+++ challenges-3/kata-514b92a657cdc65150000006.js
@@ -0,0 +1,14 @@
+// link: https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+//my solution:
+function solution(number) {
+    let sum = 0;
+
+    for (let i = 1; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
