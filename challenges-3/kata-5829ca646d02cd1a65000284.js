d6149b25f709f188be315092f3ae3645517c01f3
Haroon <haroon.ali.1104@gmail.com>
2023-09-20 19:58:17 +0100

    add kata 5829ca646d02cd1a65000284

---
+++ challenges-3/kata-5829ca646d02cd1a65000284.js
@@ -0,0 +1,39 @@
+// link: https://www.codewars.com/kata/5829ca646d02cd1a65000284/train/javascript
+
+//my solution:
+function isAgeDiverse(list) {
+    console.log(list);
+
+    //   let find = list.find(dev => dev.age >= 0 && dev.age <= 10);
+    //   if (!find) return true;
+
+    for (let i = 1; i <= 9; i++) {
+        const find = list.find(dev => dev.age.toString()[0] === i.toString());
+        if (!find) return false;
+    }
+
+    find = list.find(dev => dev.age >= 100);
+    if (!find) return false;
+
+    return true;
+}
+
+const list1 = [
+    { firstName: 'Harry', lastName: 'K.', country: 'Brazil', continent: 'Americas', age: 19, language: 'Python' },
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'JavaScript' },
+    { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+    { firstName: 'Noa', lastName: 'A.', country: 'Israel', continent: 'Asia', age: 40, language: 'Ruby' },
+    { firstName: 'Andrei', lastName: 'E.', country: 'Romania', continent: 'Europe', age: 59, language: 'C' },
+    { firstName: 'Maria', lastName: 'S.', country: 'Peru', continent: 'Americas', age: 60, language: 'C' },
+    { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 75, language: 'Python' },
+    { firstName: 'Chloe', lastName: 'K.', country: 'Guernsey', continent: 'Europe', age: 88, language: 'Ruby' },
+    { firstName: 'Viktoria', lastName: 'W.', country: 'Bulgaria', continent: 'Europe', age: 98, language: 'PHP' },
+    { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'JavaScript' }
+];
+
+const list2 = [
+    { firstName: 'Kseniya', lastName: 'T.', country: 'Belarus', continent: 'Europe', age: 29, language: 'Ruby' },
+    { firstName: 'Amar', lastName: 'V.', country: 'Bosnia and Herzegovina', continent: 'Europe', age: 32, language: 'Ruby' },
+];
+
+isAgeDiverse(list1);
